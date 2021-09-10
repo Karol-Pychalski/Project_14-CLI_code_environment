26d2dbd0016e55154287e484179921100947b80c
karol Pychalski <k.p201x@gmail.com>
2021-09-10 10:16:56 +0200

    Creating useEffect hook to fix window expanding issue

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,6 +1,7 @@
 //creating component in video 139:
 
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 interface ResizableProps {
@@ -10,24 +11,53 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> =({direction, children}) => {
   let resizableProps: ResizableBoxProps;
 
-  //'if' logic added in video 148:
-  //settings of code editor:
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+
+  //useEffect hook:
+  //using 'width' piece of state inside useEffect hook
+  useEffect(() => {
+    let timer: any;
+    //added in vid.156 (refers to debouncing)
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) { //used to keep 75% of window size reserved for coding field (black field) when user changes window width (vid. 158)
+          setWidth(window.innerWidth * 0.75);
+        }
+      }, 100);
+    };
+    window.addEventListener('resize', listener);
+
+    return ()=> {
+      window.removeEventListener('resize', listener);
+    };
+  }, [width]);  //dependency array
+
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: width, //comes from piece of state declared in const [width, setWidth]
       resizeHandles: ['e'],
+      onResizeStop: (event, data) => {  //used to avoid jumping coding field width when user changes window width
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
       minConstraints: [Infinity, 24],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       height: 300,
       width: Infinity,
-      resizeHandles: ['s'],
+      resizeHandles: ['s'], 
     };
   }
   
