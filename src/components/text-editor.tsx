0e3aa288a3ede14c20f2c0e89f056ac875cf7386
karol Pychalski <k.p201x@gmail.com>
2021-09-12 23:48:32 +0200

    CSS styling improvement to text-editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,32 +1,40 @@
 //created in video 169:
-import { useState, useEffect } from "react";
+import './text-editor.css';
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null); //HTMLDivElement - type annotation, (null)-default starting value
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('#Header');
 
   useEffect (() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) {
+        return;
+      }
       setEditing(false);
     };
     document.addEventListener('click', listener, {capture: true});
 
     return () => {
       document.removeEventListener('click', listener, {capture: true});
-    }
+    };
   }, []);
 
   if (editing) {
     return (
-      <div>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
     );
   }
 
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={'# Header'} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
