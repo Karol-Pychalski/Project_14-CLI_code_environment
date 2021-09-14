4b2310368c43a68e0a4bf216afcb914d9d2b90dd
karol Pychalski <k.p201x@gmail.com>
2021-09-14 12:25:16 +0200

    Creating implementations to action-creators file

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,12 +6,49 @@ import {
   MoveCellAction,
   InsertCellBeforeAction,
 } from '../actions';
+import { CellTypes } from '../cell'; //necessary to set type in insertCellBefore - video 189
 
 //adding types here is not obligatory but will help TS to understand what kind of value will be returned
-export const updateCell = (): UpdateCellAction => {};
+//this is updateCell action creator implementation (video 189):
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
 
-export const deleteCell = (): DeleteCellAction => {};
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
 
-export const moveCell = (): MoveCellAction => {};
+export const moveCell = (
+  id: string,
+  direction: 'up' | 'down'
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
 
-export const insertCellBefore = (): InsertCellBeforeAction => {};
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType,
+    },
+  };
+};
