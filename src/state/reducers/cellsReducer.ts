0a16dd156623cc170556d86e497d8555db69a2b3
karol Pychalski <k.p201x@gmail.com>
2021-09-15 00:59:57 +0200

    Adding immer package and basic logic to reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,5 @@
+import produce from 'immer'; //video 193
+import { forInStatement } from 'jscodeshift';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -18,22 +20,60 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+//to code inside return - while working with Redux for making a change we dont want to directly modify state object, instead we want to return a brand new object
+//inside reducers is 'update logic'
+//adding immer package in video 193
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
-      return state;
+      const { id, content } = action.payload;
+      state.data[id].content = content;
+      return;
+
     case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
+      return;
+
+    //creating swapping logic:
+    case ActionType.MOVE_CELL: //video 195
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return;
+
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: '',
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
       return state;
     default:
       return state;
   }
+});
+
+//explained in video 196:
+const randomId = () => {
+  return Math.random().toString(36).substr(2, 5);
 };
 
 export default reducer;
