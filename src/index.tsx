10408de89d434d44ea4e663f5d36b521f4bd25c2
karol Pychalski <k.p201x@gmail.com>
2021-09-16 02:37:58 +0200

    Creating hooks and cell-list component

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; //video 187
 import {store} from './state'; //video 187
 //import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list'; //video 199
 
 const App = () => {
   //onChange in CodeEditor - callback function to what a user will type (video 126) - set in code-editor.tsx in interface
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
